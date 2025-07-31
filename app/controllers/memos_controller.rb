e1dd9e283fac8dbf28624fadc30d373947f60ce6
Sh1n-TERA <sh1ng0522.t0522s@gmail.com>
2025-07-31 18:32:10 +0900

    feat：新規メモ作成機能実装（new,createアクション）

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,11 +1,21 @@
 class MemosController < ApplicationController
+  # ログインユーザーのみがメモ作成できる
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
   end
 
   def new
+    @memo = Memo.new # 新しいMemoオブジェクト生成し、ビューに渡す
   end
 
   def create
+    @memo = current_user.memos.build(memo_params) # 現ユーザーに紐付けてメモ作成
+    if @memo.save
+      redirect_to root_path, notice: 'メモが正常に作成されました。' # 成功したらトップページへリダイレクト
+    else
+      render :new, status: :unprocessable_entity # 失敗したら新規作成フォーム再表示
+    end 
   end
 
   def show
@@ -18,5 +28,13 @@ class MemosController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  # ストロングパラメーターの設定
+  # フォームからの送信データを安全に受け取るために必要
+  def memo_params
+    params.require(:memo).permit(:mode, :title, :content, :error_content, :cause, :solution, :tag, :image)
   end
 end
