a299ce90e9b46cbab2f3013a7a565989384666d4
Sh1n-TERA <sh1ng0522.t0522s@gmail.com>
2025-07-31 14:38:56 +0900

    feat：userモデルとmemoモデルにアソシエーションとバリデェーション設定

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,3 +1,25 @@
 class Memo < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  # バリデーションの設定
+  ## modeカラムとtagカラムは必須
+  with_options presence: true do
+    validates :mode
+    validates :tag
+  end
+
+  ## mode 'normal'のバリデーション
+  with_options if: -> { mode == 'normal' } do
+    validates :title  , presence: true
+    validates :content, presence: true
+  end
+  
+  ## mode 'error'のバリデーション
+  with_options if: -> { mode == 'error' } do
+    validates :error_content, presence: true
+    validates :cause        ,presence: true
+    validates :solution     ,presence: true
+  end
+
 end
