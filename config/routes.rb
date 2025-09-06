0d8ecde2b3f440688d7224f725316ea6bc561772
Sh1n-TERA <sh1ng0522.t0522s@gmail.com>
2025-09-07 00:01:53 +0900

    Fix: カレンダーページのルーティング及びタイプミス修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'memos#index'
+  get 'memos/calendar', to: 'memos#calendar'
   resources :memos
   get 'all_memos', to: 'memos#all_memos'
   delete '/images/:id', to: 'memos#delete_image', as: 'delete_image'
-  get 'memos/calender', to: 'memo#calender'
 end
