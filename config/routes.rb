3c070d6362c862e8c60cf05332584d0d47144f3b
Sh1n-TERA <sh1ng0522.t0522s@gmail.com>
2025-09-10 00:55:26 +0900

    fix：日付リンクのエラーを修正し、アンダーラインを削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'memos#index'
   get 'memos/calendar', to: 'memos#calendar'
   resources :memos
+  get 'memos/on_date(/:date)', to: 'memos#show_on_date', as: 'memos_on_date'
   get 'all_memos', to: 'memos#all_memos'
   delete '/images/:id', to: 'memos#delete_image', as: 'delete_image'
 end
